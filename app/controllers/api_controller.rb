b688e24845aff8f0cd4920e4f9f331c9dc23fe98
Rafael Cadena <rafaeljcadena@gmail.com>
2017-06-12 17:42:58 -0300

    join and unjoin team3

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,8 @@
 class ApiController < ApplicationController
 	before_action :authenticate_user!
-	# protect_from_forgery prepend: true, with: :exception
-	# protect_from_forgery prepend: true
-	protect_from_forgery with: :null_session
+	# protect_from_forgery with: :null_session
+  skip_before_filter :verify_authenticity_token, only: [:join_team, :unjoin_team]
+
 
 
 	def usuarios
